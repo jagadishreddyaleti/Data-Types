f73db0ff41c4a6edd57dbcc39abda9f086c30cbc
jagadishreddyaleti <111480451+jagadishreddyaleti@users.noreply.github.com>
2022-08-26 09:12:34 +0530

    Add files via upload

---
+++ Data_Types.py
@@ -0,0 +1,96 @@
+''' Monday 22/08/2022 '''
+
+# print(type(3)) 
+# print(type(-9)) 
+# print(type(5.6))   
+# print(type(.3)) 
+# print(type('hello world')) 
+# print(type("hello world"))
+# print(type('''hello world'''))
+# print(type("""hello world"""))
+# print(type(None))
+# print(type(True))
+# print(type(False))
+# print(5>2)
+# print(5<2)
+
+""" Tuesday 23/08/2022 """
+
+''' list data type '''
+# print(type([]))
+# print(type([1,2]))
+# print(type([4,56]))
+
+''' tuple data type '''
+# a=1
+# print(a,type(a))
+# b=2,
+# print(b,type(b))
+# c=4,5,6
+# print(c,type(c))
+# print(type((7,)))
+# print(type((7,8,9)))
+
+""" set data type """
+
+# a=[1,2,3]
+# print(a,type(a))
+# a.clear()
+# print(a)
+
+# b={1,'hello',5.6}
+# print(b,type(b))
+# b.clear()
+# print(b)
+
+# c={}
+# print(c,type(c))
+# d={1:'hello'}
+# print(d,type(d))
+# d.clear()
+# print(d)
+
+''' wednesday 24/08/22'''
+
+'''byte data type'''
+
+# print(type(b'hello'))
+
+'''type of strings'''
+
+'''raw string '''
+# print(type(r'hello\new'))
+# print('hello\new')
+# print(r'hello\new')
+
+''' fast string'''
+# a='python'
+# print('hello{a}')
+# print(f'hello{a}')
+
+'''complex data type'''
+# print(type(2+3j))
+# print(type(3j))
+# print(type(2))
+
+a=6
+# print(a,(type(a)))
+# print(float(a))
+# print('%f' %a)
+# print('%.4f' %a)
+
+b=7.4
+# print(type(b))
+# # print(int(b))
+# # print('%i' %b)
+# # print('%f'%b )
+# print('%.4f'%b )
+# print('%f'%b )
+
+b=0.2+0.4
+# print(b,type(b))
+# print('%0.4f' %b)
+# print(int(b))
+print('%.1f' %b)
+print('%.2f' %b)
+print(float(b))
